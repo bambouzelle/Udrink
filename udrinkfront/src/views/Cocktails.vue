e1dceec372309524ee08575a17c3b5d5af880877
Jonathann Jouvin <jonathan.jouvin@epitech.eu>
2023-04-11 17:48:11 +0200

    ajout infinite scroll logique

--- udrinkfront/src/views/Cocktails.vue
+++ udrinkfront/src/views/Cocktails.vue
@@ -7,9 +7,11 @@
 			</div>
 		</div>
 
-		<div v-for="i in data" class="row" :key="i" style="margin-top: 4em;">
-			<CocktailCard :cocktail="i"/>
+		<div v-for="cocktail in dataToShow" class="row" :key="cocktail" style="margin-top: 2em;">
+			<CocktailCard :cocktail="cocktail"/>
 		</div>
+
+		<div v-if="noMore"> Vous avez vu tout les cocktails !</div>
 	</div>
 </template>
 
@@ -26,8 +28,48 @@ export default {
 				{ id: 2, title: "Cocktail2", ingredients: ["I1", "I2", "I3"], description: "HOW TO DO IT GOOF", glass: "ballon" },
 				{ id: 3, title: "Cocktail3", ingredients: ["I1", "I2", "I3", "I4"], description: "HOW TO DO IT GOOFyyy", glass: "whiskey" },
 				{ id: 4, title: "Cocktail3", ingredients: ["I3", "I4"], description: "HOW TO DO IT GOOFyyy", glass: "shooter" },
+				{ id: 5, title: "Cocktail3", ingredients: ["I2", "I3", "I4"], description: "HOW TO DO IT GOOFyyy", glass: "biere" },
+				{ id: 1, title: "Cocktail1", ingredients: ["I1", "I2"], description: "HOW TO DO IT", glass: "coupe" },
+				{ id: 2, title: "Cocktail2", ingredients: ["I1", "I2", "I3"], description: "HOW TO DO IT GOOF", glass: "ballon" },
+				{ id: 3, title: "Cocktail3", ingredients: ["I1", "I2", "I3", "I4"], description: "HOW TO DO IT GOOFyyy", glass: "whiskey" },
+				{ id: 4, title: "Cocktail3", ingredients: ["I3", "I4"], description: "HOW TO DO IT GOOFyyy", glass: "shooter" },
+				{ id: 5, title: "Cocktail3", ingredients: ["I2", "I3", "I4"], description: "HOW TO DO IT GOOFyyy", glass: "biere" },
+				{ id: 1, title: "Cocktail1", ingredients: ["I1", "I2"], description: "HOW TO DO IT", glass: "coupe" },
+				{ id: 2, title: "Cocktail2", ingredients: ["I1", "I2", "I3"], description: "HOW TO DO IT GOOF", glass: "ballon" },
+				{ id: 3, title: "Cocktail3", ingredients: ["I1", "I2", "I3", "I4"], description: "HOW TO DO IT GOOFyyy", glass: "whiskey" },
+				{ id: 4, title: "Cocktail3", ingredients: ["I3", "I4"], description: "HOW TO DO IT GOOFyyy", glass: "shooter" },
 				{ id: 5, title: "Cocktail3", ingredients: ["I2", "I3", "I4"], description: "HOW TO DO IT GOOFyyy", glass: "biere" }
-			]
+			],
+			dataToShow:[],
+			noMore: false
+		}
+	},
+	mounted(){
+		this.getMoreData();
+		this.getNextCocktails();
+	},
+	methods:{
+		getNextCocktails(){
+			window.onscroll = () => {
+				let bottomOfWindow = document.documentElement.scrollTop + window.innerHeight === document.documentElement.offsetHeight;
+
+				if (bottomOfWindow) {
+						console.log('rendu en bas')
+						this.getMoreData()
+				}
+			};
+		},
+		getMoreData(){
+			let i = this.dataToShow.length;
+			let imax = i+10;
+			for ( i ; i<imax ; i++){
+				if (this.data[i]){
+					this.dataToShow.push(this.data[i])
+				}
+				else{
+					this.noMore = true
+				}
+			}
 		}
 	}
 }
