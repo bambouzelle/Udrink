5da834d0ff91bbf05d208a25e0635c7d8d93f7e6
Jonathann Jouvin <jonathan.jouvin@epitech.eu>
2023-04-11 09:59:28 +0200

    feature/new bar : fix lateral bar

--- udrinkfront/src/components/TheLateralBar.vue
+++ udrinkfront/src/components/TheLateralBar.vue
@@ -1,19 +1,23 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-			<div class="container-grid border-top rounded-0">
-						<Transition name="slide" id="panel">
-							<div v-if="lateralBarShowed" class ="lateralBar panelGrid">
-								<div id="titrePanel" class="textMenu">Mes ingrédients</div>
-								<div id="contentPanel" class="textMenu"> pas d'ingrédients !</div>
-							</div>
-						</Transition>
-	
-						<img src="../assets/left-arrow.svg" style="max-height: 3em;" @click="toggleLateralBar"
-							type="button"
-							id="button"
-							class="lateralBar"
-							>
-				</div>
+	<div class="container-grid border-top rounded-0">
+		<Transition name="slide" id="panel">
+			<div v-if="lateralBarShowed" class="lateralBar panelGrid">
+				<div id="titrePanel" class="textMenu">Mes ingrédients</div>
+				<div id="contentPanel" class="textMenu"> pas d'ingrédients !</div>
+			</div>
+		</Transition>
+
+		<div ref="buttonBar" class="lateralBar" id="button">
+			<img ref="image" v-show="lateralBarShowed" src="../assets/left-arrow.svg" style="max-height: 3em;"
+				@click="toggleLateralBar" type="button">
+
+			<img v-show="!lateralBarShowed" src="../assets/ingredients.png" style="max-height: 3em;" @click="toggleLateralBar"
+				type="button">
+
+		</div>
+
+	</div>
 </template>
 
 <script>
@@ -21,21 +25,35 @@ export default {
 	name: "NavBar",
 	data() {
 		return {
-			lateralBarShowed: false
+			lateralBarShowed: false,
+			buttonOffset: 0,
+			buttonWidth: 48,
 		}
 	},
 	created() {
 		this.lateralBarShowed = false
 	},
+	mounted() {
+		this.buttonOffset = this.$refs.buttonBar.getBoundingClientRect().left;
+		this.$refs.buttonBar.style.transition = '0s'
+		this.$refs.buttonBar.style.transform = 'translate(-' + this.buttonOffset + 'px)';
+		window.onresize = () => {
+			this.$router.go(0)
+		}
+	},
 	methods: {
 		toggleLateralBar() {
+			this.$refs.buttonBar.style.transition = 'all 0.3s ease-in-out'
+			if (this.$refs.image.width != 0) {
+				this.$refs.buttonBar.width = this.$refs.image.width
+			}
 			this.lateralBarShowed = !this.lateralBarShowed
-			//const button = document.getElementById('button')
-			if (this.lateralBarShowed){
-				//button.style.transform = "translate(100%)"
-			} 
+			if (this.lateralBarShowed) {
+				this.$refs.buttonBar.style.transform = "translate(+" + 0 + "px)";
+			}
+
 			else {
-				//button.style.transform = "translate(0px)"
+				this.$refs.buttonBar.style.transform = "translate(-" + this.buttonOffset + "px)";
 			}
 		}
 	},
@@ -56,45 +74,37 @@ export default {
 	z-index: 999;
 }
 
-.panelGrid{
+.panelGrid {
 	display: grid;
 	grid-template-rows: 1fr 30fr;
 }
 
-#titrePanel{
+#titrePanel {
 	grid-row-start: 1;
 	grid-row-end: 2;
 }
 
 #panel {
 	grid-column-start: 1;
-  grid-column-end: 2;
+	grid-column-end: 2;
 	grid-row-start: 1;
 	grid-row-end: 3;
 }
 
-#contentPanel{
+#contentPanel {
 	grid-row-start: 2;
 	grid-row-end: 3;
 }
 
 #button {
 	grid-column-start: 2;
-  grid-column-end: 3;
+	grid-column-end: 3;
 	grid-row-start: 1;
 	grid-row-end: 2;
 	border-start-end-radius: 55%;
-	border-end-end-radius:55%;
-	transform: translateX(-452%);
-	transition-delay: 0.25s;
-	transition: transform 0.3s ease-in-out;
-	position: relative;
-}
-
-.buttonLeft{
-	transform: translateX(452%);
-	transition-delay: 0.25s;
-	transition: transform 0.3s ease-in-out;
+	border-end-end-radius: 55%;
+	transition: all 0.3s ease-in-out;
+	width: 46px;
 }
 
 .lateralBar {
@@ -105,11 +115,6 @@ export default {
 .slide-enter-active,
 .slide-leave-active {
 	transition: all 0.3s ease-in-out;
-}
-
-/* delay leave of parent element */
-.slide-leave-active {
-	transition-delay: 0.25s;
 }
 
 .slide-enter-from,
