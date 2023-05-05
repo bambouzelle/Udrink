85b3f5575d22a265760ade4d0e841af21d605998
MEUNIER Adrien <adrien.meunier42@gmail.com>
2023-05-05 15:32:52 +0200

    back de nouveau fonctionnel

--- udrinkback/backend/settings.py
+++ udrinkback/backend/settings.py
@@ -142,8 +142,11 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
-       'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-    ),
+    
+    'rest_framework.authentication.SessionAuthentication',
+    'rest_framework.authentication.BasicAuthentication',
+    'rest_framework_simplejwt.authentication.JWTAuthentication', 
+),
 }
 
 AUTH0_DOMAIN = 'authudrink.uk.auth0.com'
